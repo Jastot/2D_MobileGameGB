ab150734534fc85a915b80280a6e40c9746edfb6
Jastot <george1005@yandex.ru>
2021-04-14 14:33:48 +0300

    FirstHomeWork

---
+++ Assets/Script/Game/InputGame/InputTapView.cs
@@ -0,0 +1,35 @@
+﻿using Tools;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.InputLogic
+{
+    internal sealed class InputTapView : BaseInputView
+    {
+        private int idR;
+        private int idL;
+        public override void Init(SubscriptionProperty<float> leftMove, SubscriptionProperty<float> rightMove,
+            float speed)
+        {
+            base.Init(leftMove, rightMove, speed);
+            idR = _buttonMoveRight.GetInstanceID();
+            idL = _buttonMoveLeft.GetInstanceID();
+            _buttonMoveRight.onClick.AddListener(()=>OnClick(_buttonMoveRight.GetInstanceID()));
+            _buttonMoveLeft.onClick.AddListener(()=>OnClick(_buttonMoveLeft.GetInstanceID()));
+        }
+
+        [SerializeField] 
+        private Button _buttonMoveRight;
+        [SerializeField] 
+        private Button _buttonMoveLeft;
+
+        private void OnClick(int id)
+        {
+            if(id == idR)
+                OnRightMove(Time.deltaTime * _speed);
+            if(id == idL)
+                OnLeftMove(Time.deltaTime *_speed);
+        }
+    }
+}
+
---
+++ Assets/Script/Game/InputGame/TrailCursor.cs
@@ -0,0 +1,51 @@
+﻿using JoostenProductions;
+using Tools;
+using UnityEditor.PackageManager.UI;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace Game.InputLogic
+{
+    internal sealed  class TrailCursor: BaseInputView, IBeginDragHandler, IEndDragHandler, IDragHandler
+    {
+        private float _threshold = 40;
+        private Vector2 _startPosition;
+        [SerializeField] private TrailRenderer _trailRenderer;
+        [SerializeField] private GameObject _trail;
+        private Camera _camera;
+        public override void Init(SubscriptionProperty<float> leftMove, SubscriptionProperty<float> rightMove, float speed)
+        {
+            base.Init(leftMove, rightMove, speed);
+            _camera = Camera.main;
+        }
+        public void OnDrag(PointerEventData eventData)
+        {
+            float diff = eventData.position.x - _startPosition.x;
+            if (Mathf.Abs(diff) >= _threshold)
+            {
+                if(diff > 0)
+                    OnRightMove(_speed * Time.deltaTime);
+                else
+                    OnLeftMove(-_speed * Time.deltaTime);
+            }
+
+            var trailX = eventData.position.x / _camera.pixelWidth;
+            var trailY = eventData.position.y / _camera.pixelHeight;
+            _trail.transform.position = new Vector3(trailX, trailY, 0);
+            Debug.Log(trailX);
+            Debug.Log(trailY);
+        }
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            _startPosition = eventData.position;
+            _trailRenderer.emitting = true;
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            _trailRenderer.emitting = false;
+        }
+    }
+}
