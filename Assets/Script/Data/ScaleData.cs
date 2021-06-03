91b3ec3d60a9617a86c2ee9dd4a83ca5b3c93afe
Jastot <george1005@yandex.ru>
2021-06-03 20:58:34 +0300

    TweensAreAwesome!!!

---
+++ Assets/Script/Data/ScaleData.cs
@@ -0,0 +1,15 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Script.Data
+{
+    [CreateAssetMenu(fileName = "Scale Data",
+        menuName = "Data/Scale Data")]
+    public sealed class ScaleData : ScriptableObject
+    {
+        public Ease Ease;
+        [Range(0f, 5f)] 
+        public float Duration;
+        public Vector2 Scale;
+    }
+}
